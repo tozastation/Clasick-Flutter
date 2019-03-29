85b82fc0d22ca9238e8907e5a4063cd6fc2c7251
tozastation <b1016031@fun.ac.jp>
2019-03-29 14:27:54 +0900

    [Feature] add login display

---
+++ lib/screens/login.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => new _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clasick'),
+      ),
+      body: new Container(
+          padding: const EdgeInsets.all(16.0),
+          margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: new TextFormField(
+                  decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.teal)),
+                      labelText: 'User ID',
+                      hintText: 'Please enter a search term'),
+                ),
+              ),
+              new Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: new TextFormField(
+                  decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                          borderSide: new BorderSide(color: Colors.teal)),
+                      labelText: 'Password',
+                      hintText: 'Please enter a search term'),
+                ),
+              ),
+              new Container(
+                  margin: const EdgeInsets.all(4.0),
+                  child: ButtonTheme(
+                    minWidth: 80.0,
+                    height: 40.0,
+                    child: new RaisedButton(
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      color: Colors.green,
+                      shape: RoundedRectangleBorder(),
+                      onPressed: () {},
+                    ),
+                  )),
+            ],
+          )),
+    );
+  }
+}

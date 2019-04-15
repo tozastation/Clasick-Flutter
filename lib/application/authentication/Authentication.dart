4cd2ac97fd7170a87a85e6635a81e781274a392b
tozastation <b1016031@fun.ac.jp>
2019-04-15 21:27:17 +0900

    [Feature] add login & authentication flow

---
+++ lib/application/authentication/Authentication.dart
@@ -0,0 +1,3 @@
+export 'package:clasick_flutter/application/authentication/AuthenticationState.dart';
+export 'package:clasick_flutter/application/authentication/AuthenticationEvent.dart';
+export 'package:clasick_flutter/application/authentication/AuthenticationBloc.dart';
---
+++ lib/application/login/Login.dart
@@ -0,0 +1,4 @@
+export 'package:clasick_flutter/application/login/LoginState.dart';
+export 'package:clasick_flutter/application/login/LoginEvent.dart';
+export 'package:clasick_flutter/application/login/LoginBloc.dart';
+export 'package:clasick_flutter/application/login/LoginScreen.dart';
